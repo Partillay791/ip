7ae75c25e4e0170322c184546e336c0a32607ecd
Partillay791 <226736716+Partillay791@users.noreply.github.com>
2025-08-23 01:43:22 +0800

    Add marking functions for tasks

--- src/main/java/Sunoo.java
+++ src/main/java/Sunoo.java
@@ -2,8 +2,8 @@ import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Sunoo {
-    private static final String HORIZONTAL_LINE = "_".repeat(40);
-    private static final ArrayList<String> taskList = new ArrayList<>();
+    private static final String HORIZONTAL_LINE = "_".repeat(50);
+    private static final ArrayList<Task> taskList = new ArrayList<>();
 
     public static void main(String[] args) {
         String logo = " ____  _   _ _   _  ____  ____  \n"
@@ -25,6 +25,12 @@ public class Sunoo {
                 break;
             } else if (userInput.equals("list")) {
                 listTasks();
+            } else if (userInput.startsWith("mark ")) {
+                int taskIndex = Integer.parseInt(userInput.substring("mark ".length()));
+                markTask(taskIndex);
+            } else if (userInput.startsWith("unmark ")) {
+                int taskIndex = Integer.parseInt(userInput.substring("unmark ".length()));
+                unmarkTask(taskIndex);
             } else {
                 addTask(userInput);
             }
@@ -40,16 +46,33 @@ public class Sunoo {
     }
 
     private static void addTask(String userInput) {
-        taskList.add(userInput);
+        taskList.add(new Task(userInput));
         System.out.println(HORIZONTAL_LINE);
         System.out.println("added: " + userInput);
         System.out.println(HORIZONTAL_LINE);
     }
 
+    private static void markTask(int taskIndex) {
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Nice job, ENGENE! I've marked this task as done:");
+        taskList.get(taskIndex - 1).markAsDone();
+        System.out.println(taskList.get(taskIndex - 1).getFullDescription());
+        System.out.println(HORIZONTAL_LINE);
+    }
+
+    private static void unmarkTask(int taskIndex) {
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Ok, ENGENE! I've marked this task as not done yet:");
+        taskList.get(taskIndex - 1).markAsNotDone();
+        System.out.println(taskList.get(taskIndex - 1).getFullDescription());
+        System.out.println(HORIZONTAL_LINE);
+    }
+
     private static void listTasks() {
         System.out.println(HORIZONTAL_LINE);
+        System.out.println("ENGENE, here are the tasks recorded by ddeonu:");
         for (int i = 1; i <= taskList.size(); i++) {
-            System.out.println(i + ". " + taskList.get(i - 1));
+            System.out.println(i + ". " + taskList.get(i - 1).getFullDescription());
         }
         System.out.println(HORIZONTAL_LINE);
     }
