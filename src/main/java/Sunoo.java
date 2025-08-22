a7f06e14c98d970e808b968d138957041f04cbfd
Partillay791 <226736716+Partillay791@users.noreply.github.com>
2025-08-22 22:20:12 +0800

    Rename and add speeches of greet and exit

---
+++ src/main/java/Sunoo.java
@@ -0,0 +1,17 @@
+public class Sunoo {
+    private static final String HORIZONTAL_LINE = "_".repeat(40);
+
+    public static void main(String[] args) {
+        String logo = " ____  _   _ _   _  ____  ____  \n"
+                + "/ ___|| | | | \\ | ||  _ \\|  _ \\ \n"
+                + "\\___ \\| | | |  \\| || | | | | | |\n"
+                + " ___) | |_| | |\\  || |_| | |_| |\n"
+                + "|____/ \\___/|_| \\_||____/|____/ \n";
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Hello, this is Sunoo!\n" + logo);
+        System.out.println("I'm glad to help you!");
+        System.out.println(HORIZONTAL_LINE);
+        System.out.println("Bye bye!");
+        System.out.println(HORIZONTAL_LINE);
+    }
+}
