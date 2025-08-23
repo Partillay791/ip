6d804ac0d5ed75e3df3c2d38efeecf9561a4bced
Partillay791 <226736716+Partillay791@users.noreply.github.com>
2025-08-23 16:19:53 +0800

    Add exception class to represent exceptions

--- src/main/java/SunooException.java
+++ src/main/java/SunooException.java
@@ -1,7 +1,5 @@
-package PACKAGE_NAME;
-
 public class SunooException extends RuntimeException {
-  public SunooException(String message) {
-    super(message);
-  }
+    public SunooException(String message) {
+        super(message);
+    }
 }
