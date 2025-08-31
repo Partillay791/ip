a3b45cb678f40f35718b94993f411c28e7f855ac
Partillay791 <226736716+Partillay791@users.noreply.github.com>
2025-08-31 16:21:13 +0800

    Update Parser to return Command objects

--- src/main/java/Parser.java
+++ src/main/java/Parser.java
@@ -2,107 +2,69 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class Parser {
-    public static void parse(String userInput) {
-        if (userInput.equals("bye")) {
-            Ui.showExitMessage();
-            Sunoo.performExitNext();
-
-        } else if (userInput.equals("list")) {
-            Ui.showListTasksMessage();
-
-
-        } else if (userInput.startsWith("mark")) {
-            if (userInput.equals("mark")) {
-                throw new SunooException("ENGENE, ddeonu does not know what to mark!");
-            }
-            if (!userInput.startsWith("mark ")) {
-                throw new SunooException("ENGENE, leave a space after \"mark\"!");
-            }
-            int taskIndex;
+    public static Command parse(String userInput) {
+        String[] parts = userInput.split(" ", 2);
+        String command = parts[0];
+        int taskIndex;
+        switch (command) {
+        case "bye":
+            return new ByeCommand();
+        case "list":
+            return new ListCommand();
+        case "mark":
             try {
-                taskIndex = Integer.parseInt(userInput.substring("mark ".length()));
+                taskIndex = Integer.parseInt(parts[1]);
             } catch (NumberFormatException e) {
                 throw new SunooException("ENGENE, I need a number to mark!");
             }
-            TaskList.markTask(taskIndex);
-            Ui.showMarkTaskMessage(taskIndex);
-
-
-        } else if (userInput.startsWith("unmark")) {
-            if (userInput.equals("unmark")) {
-                throw new SunooException("ENGENE, ddeonu does not know what to unmark!");
-            }
-            if (!userInput.startsWith("unmark ")) {
-                throw new SunooException("ENGENE, leave a space after \"unmark\"!");
-            }
-            int taskIndex;
+            return new MarkCommand(taskIndex);
+        case "unmark":
             try {
-                taskIndex = Integer.parseInt(userInput.substring("unmark ".length()));
+                taskIndex = Integer.parseInt(parts[1]);
             } catch (NumberFormatException e) {
-                throw new SunooException("ENGENE, I need a number to unmark!");
+                throw new SunooException("ENGENE, I need a number to mark!");
             }
-            TaskList.unmarkTask(taskIndex);
-            Ui.showUnmarkTaskMessage(taskIndex);
-
-
-        } else if (userInput.startsWith("delete")) {
-            if (userInput.equals("delete")) {
-                throw new SunooException("ENGENE, ddeonu does not know what to delete!");
-            }
-            if (!userInput.startsWith("delete ")) {
-                throw new SunooException("ENGENE, leave a space after \"delete\"!");
-            }
-            int taskIndex;
+            return new UnmarkCommand(taskIndex);
+        case "delete":
             try {
-                taskIndex = Integer.parseInt(userInput.substring("delete ".length()));
+                taskIndex = Integer.parseInt(parts[1]);
             } catch (NumberFormatException e) {
                 throw new SunooException("ENGENE, I need a number to delete!");
             }
-            Task deletedTask = TaskList.deleteTask(taskIndex);
-            Ui.showDeleteTaskMessage(deletedTask);
-
-
-        } else if (userInput.startsWith("todo")) {
-            if (userInput.equals("todo")) {
+            return new DeleteCommand(taskIndex);
+        case "todo":
+            String todoDescription;
+            try {
+                todoDescription = parts[1];
+            } catch (ArrayIndexOutOfBoundsException e) {
                 throw new SunooException("Sorry ENGENE, you don't have a todo description!");
             }
-            if (!userInput.startsWith("todo ")) {
-                throw new SunooException("ENGENE, leave a space after \"todo\"!");
-            }
-            Task addedTask = TaskList.addTask(new ToDo(false, userInput.substring(5)));
-            Ui.showAddTaskMessage(addedTask);
-
-
-        } else if (userInput.startsWith("deadline")) {
-            if (userInput.equals("deadline")) {
+            return new AddCommand(new ToDo(false, todoDescription));
+        case "deadline":
+            String deadlineDescription;
+            try {
+                deadlineDescription = parts[1];
+            } catch (ArrayIndexOutOfBoundsException e) {
                 throw new SunooException("Sorry ENGENE, your deadline task is empty!");
             }
-            if (!userInput.startsWith("deadline ")) {
-                throw new SunooException("ENGENE, leave a space after \"deadline\"!");
-            }
-            userInput = userInput.substring(9);
-            String[] splitResult = userInput.split(" /by ");
+            String[] splitResult = deadlineDescription.split(" /by ");
             if (splitResult.length < 2) {
                 throw new SunooException("""                      
                         ENGENE, there seems to be a problem!
                         1. Remember to include the " /by " keyword between your task description and deadline so I can know when it is due!
                         2. Your description and deadline cannot be empty!""");
             }
-            String taskDescription = splitResult[0];
+            String deadlineTaskDescription = splitResult[0];
             LocalDateTime deadline = localDateTimeParser(splitResult[1]);
-            Task addedTask = TaskList.addTask(new Deadline(false, taskDescription, deadline));
-            Ui.showAddTaskMessage(addedTask);
-
-
-        } else if (userInput.startsWith("event")) {
-            if (userInput.equals("event")) {
+            return new AddCommand(new Deadline(false, deadlineTaskDescription, deadline));
+        case "event":
+            String eventDescription;
+            try {
+                eventDescription = parts[1];
+            } catch (ArrayIndexOutOfBoundsException e) {
                 throw new SunooException("Sorry ENGENE, your event task is empty!");
             }
-            if (!userInput.startsWith("event ")) {
-                throw new SunooException("ENGENE, leave a space after \"event\"!");
-            }
-            userInput = userInput.substring(6);
-            String[] fromSplit = userInput.split(" /from ");
+            String[] fromSplit = eventDescription.split(" /from ");
             if (fromSplit.length < 2) {
                 throw new SunooException("""                      
                         ENGENE, there seems to be a problem!
@@ -121,12 +83,9 @@ public class Parser {
             String taskDescription = fromSplit[0];
             LocalDateTime startTime = localDateTimeParser(toSplit[0]);
             LocalDateTime endTime = localDateTimeParser(toSplit[1]);
-            Task addedTask = TaskList.addTask(new Event(false, taskDescription, startTime, endTime));
-            Ui.showAddTaskMessage(addedTask);
-
-
-        } else {
-            throw new SunooException("Sorry! Ddeonu doesn't know what you mean ToT");
+            return new AddCommand(new Event(false, taskDescription, startTime, endTime));
+        default:
+            return new IncorrectCommand("Sorry! Ddeonu doesn't know what you mean ToT");
         }
     }
 
@@ -134,4 +93,5 @@ public class Parser {
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm");
         return LocalDateTime.parse(input, formatter);
     }
+
 }
