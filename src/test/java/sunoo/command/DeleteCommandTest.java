007b5a275566fdd70061e662caf3f6282e2d856e
Partillay791 <226736716+Partillay791@users.noreply.github.com>
2025-09-02 20:27:36 +0800

    Add JUnit tests for Parser and DeleteCommand

---
+++ src/test/java/sunoo/command/DeleteCommandTest.java
@@ -0,0 +1,53 @@
+package sunoo.command;
+
+import org.junit.jupiter.api.Test;
+
+import sunoo.exception.SunooException;
+
+import sunoo.task.Deadline;
+import sunoo.task.TaskList;
+import sunoo.task.ToDo;
+
+import java.time.LocalDateTime;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.fail;
+
+public class DeleteCommandTest {
+
+    @Test
+    public void execute_zeroIndex_exceptionThrown() {
+        try {
+            TaskList taskList = new TaskList();
+            taskList.addTask(new ToDo(false, "homework"));
+            new DeleteCommand(0).execute(taskList);
+            fail();
+        } catch (SunooException e) {
+            assertEquals("Sorry ENGENE, that's not a valid task index!", e.getMessage());
+        }
+    }
+
+    @Test
+    public void execute_negativeIndex_exceptionThrown() {
+        try {
+            TaskList taskList = new TaskList();
+            taskList.addTask(new ToDo(false, "study"));
+            new DeleteCommand(-5).execute(taskList);
+            fail();
+        } catch (SunooException e) {
+            assertEquals("Sorry ENGENE, that's not a valid task index!", e.getMessage());
+        }
+    }
+
+    @Test
+    public void execute_validIndex_success() {
+        TaskList taskList = new TaskList();
+        taskList.addTask(new ToDo(true, "eat dinner"));
+        taskList.addTask(new Deadline(false, "assignment", LocalDateTime.of(2020, 10, 10, 10, 10)));
+        taskList.addTask(new ToDo(false, "return books"));
+        new DeleteCommand(3).execute(taskList);
+        assertEquals(new ToDo(true, "eat dinner"), taskList.getTask(1));
+        assertEquals(new Deadline(false, "assignment", LocalDateTime.of(2020, 10, 10, 10, 10)), taskList.getTask(2));
+        assertEquals(2, taskList.getNumTasks());
+    }
+}
