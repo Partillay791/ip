a4719098c0c7826830eb0caa7b869b6bbabfdcc0
Partillay791 <226736716+Partillay791@users.noreply.github.com>
2025-09-15 19:41:27 +0800

    Style GUI with CSS files and an image
    The application provides basic functions as a chatbot but has basic GUI.
    
    Having a better GUI potentially increthe ases favour of users towards
    the chatbot.
    
    Let's create new css files to style the main window and dialog box with
    colours and modify their fonts. Let's add an image to become the icon of
    the application.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import java.io.IOException;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 
@@ -19,7 +20,10 @@ public class Main extends Application {
             stage.setScene(scene);
 
             stage.setMinHeight(600);
-            stage.setMinWidth(417);
+            stage.setMinWidth(817);
+
+            stage.getIcons().add(new Image(Main.class.getResourceAsStream("/images/darkblood.jpg")));
+            stage.setTitle("Chat with Sunoo!");
 
             stage.show();
         } catch (IOException e) {
